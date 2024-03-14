22cb6e18ab6ed8ea783932b6a32f8b7c6d4893b4
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-03-14 12:55:48 +0500

    feat: add sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -238,8 +238,17 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  const sorted = arr.sort((a, b) => {
+    if (a.country < b.country) return -1;
+    if (a.country > b.country) return 1;
+
+    if (a.city < b.city) return -1;
+    if (a.city > b.city) return 1;
+
+    return 0;
+  });
+  return sorted;
 }
 
 /**
